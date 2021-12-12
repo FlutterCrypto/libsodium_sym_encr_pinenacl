7c559260d05c1d136a348aab030a4668b6d2fbc8
michaelfehr <michael_fehrgmx.de>
2021-12-12 23:39:18 +0100

    Initial commit

---
+++ android/app/src/main/java/de/fluttercrypto/libsodium_sym_encrypt/libsodium_sym_encr_pinenacl/MainActivity.java
@@ -0,0 +1,6 @@
+package de.fluttercrypto.libsodium_sym_encrypt.libsodium_sym_encr_pinenacl;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/secretbox_pbkdf2_encryption_route.dart
@@ -0,0 +1,393 @@
+import 'dart:typed_data';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pinenacl/api.dart';
+import 'package:pinenacl/key_derivation.dart';
+import 'package:pinenacl/x25519.dart' show SecretBox;
+
+class SecretBoxPbkdf2EncryptionRoute extends StatefulWidget {
+  const SecretBoxPbkdf2EncryptionRoute({Key? key}) : super(key: key);
+
+  final String title = 'SecretBox Verschlüsselung';
+
+
+  @override
+  _MyFormPageState createState() => _MyFormPageState();
+}
+
+class _MyFormPageState extends State<SecretBoxPbkdf2EncryptionRoute> {
+  @override
+  void initState() {
+    super.initState();
+    descriptionController.text = txtDescription;
+  }
+
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController descriptionController = TextEditingController();
+
+  // the following controller have a default value
+  TextEditingController plaintextController = TextEditingController(
+      text: 'The quick brown fox jumps over the lazy dog');
+  TextEditingController iterationenController =
+      TextEditingController(text: '15000');
+
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController outputController = TextEditingController();
+
+  String txtDescription = 'SecretBox XSalsa20Poly1305 Verschlüsselung, der Schlüssel'
+      ' wird mit PBKDF2 (wählbare Iterationen) von einem Passwort abgeleitet.';
+
+  String _returnJson(String data) {
+    var parts = data.split(':');
+    var algorithm = parts[0];
+    var iterations = parts[1];
+    var salt = parts[2];
+    //var iv = parts[3]; // nicht benutzt
+    var iv = 'nicht benutzt';
+    var ciphertext = parts[3];
+    var gcmTag = parts[4]; // nicht benutzt
+
+    JsonEncryption jsonEncryption = JsonEncryption(
+        algorithm: algorithm,
+        iterations: iterations,
+        salt: salt,
+        iv: iv,
+        ciphertext: ciphertext,
+        gcmTag: gcmTag);
+
+    String encryptionResult = jsonEncode(jsonEncryption);
+    // make it pretty
+    var object = json.decode(encryptionResult);
+    var prettyEncryptionResult2 = JsonEncoder.withIndent('  ').convert(object);
+
+    return prettyEncryptionResult2;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                //SizedBox(height: 20),
+                // form description
+                TextFormField(
+                  controller: descriptionController,
+                  keyboardType: TextInputType.text,
+                  autocorrect: false,
+                  enabled: false,
+                  // false = disabled, true = enabled
+                  maxLines: 3,
+                  decoration: InputDecoration(
+                    labelText: 'Beschreibung',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+                // plaintext
+                TextFormField(
+                  controller: plaintextController,
+                  maxLines: 3,
+                  maxLength: 500,
+                  keyboardType: TextInputType.multiline,
+                  autocorrect: false,
+                  decoration: InputDecoration(
+                    labelText: 'Klartext',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Bitte Daten eingeben';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.grey,
+                            textStyle: TextStyle(color: Colors.white)),
+                        onPressed: () {
+                          plaintextController.text = '';
+                        },
+                        child: Text('Feld löschen'),
+                      ),
+                    ),
+                    SizedBox(width: 15),
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.blue,
+                            textStyle: TextStyle(color: Colors.white)),
+                        onPressed: () async {
+                          final data =
+                              await Clipboard.getData(Clipboard.kTextPlain);
+                          plaintextController.text = data!.text!;
+                        },
+                        child: Text('aus Zwischenablage einfügen'),
+                      ),
+                    ),
+                  ],
+                ),
+
+                // password
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: passwordController,
+                  enabled: true,
+                  // false = disabled, true = enabled
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    labelText: 'Passwort',
+                    hintText: 'geben Sie das Passwort zur Verschlüsselung ein',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Bitte Daten eingeben';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 15),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.grey,
+                            textStyle: TextStyle(color: Colors.white)),
+                        onPressed: () {
+                          passwordController.text = '';
+                        },
+                        child: Text('Feld löschen'),
+                      ),
+                    ),
+                    SizedBox(width: 15),
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.blue,
+                            textStyle: TextStyle(color: Colors.white)),
+                        onPressed: () async {
+                          final data =
+                              await Clipboard.getData(Clipboard.kTextPlain);
+                          passwordController.text = data!.text!;
+                        },
+                        child: Text('aus Zwischenablage einfügen'),
+                      ),
+                    ),
+                  ],
+                ),
+
+                // iterationen
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: iterationenController,
+                  enabled: true,
+                  // false = disabled, true = enabled
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  decoration: InputDecoration(
+                    labelText: 'Iterationen',
+                    hintText: 'geben Sie die Anzahl der Iterationen ein',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        int.tryParse(value)! < 10000) {
+                      return 'Bitte Daten eingeben (Minimum 10000)';
+                    }
+                    return null;
+                  },
+                ),
+
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.grey,
+                            textStyle: TextStyle(color: Colors.white)),
+                        onPressed: () {
+                          // reset() setzt alle Felder wieder auf den Initalwert zurück.
+                          _formKey.currentState!.reset();
+                        },
+                        child: Text('Formulardaten löschen'),
+                      ),
+                    ),
+                    SizedBox(width: 25),
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.blue,
+                            textStyle: TextStyle(color: Colors.white)),
+                        onPressed: () {
+                          // Wenn alle Validatoren der Felder des Formulars gültig sind.
+                          if (_formKey.currentState!.validate()) {
+                            String plaintext = plaintextController.text;
+                            String password = passwordController.text;
+                            String iterations = iterationenController.text;
+                            String output = xSalsa20Poly1305IterPbkdf2EncryptToBase64(
+                                password, iterations, plaintext);
+                            String _formdata = 'XSalsa20Poly1305 PBKDF2' +
+                                ':' +
+                                iterations +
+                                ':' +
+                                output +
+                                ':nicht benutzt'; // empty gcm tag
+
+                            String jsonOutput = _returnJson(_formdata);
+                            outputController.text = jsonOutput;
+                          } else {
+                            print("Formular ist nicht gültig");
+                          }
+                        },
+                        child: Text('verschlüsseln'),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: outputController,
+                  maxLines: 15,
+                  maxLength: 500,
+                  decoration: InputDecoration(
+                    labelText: 'Ausgabe',
+                    hintText: 'Ausgabe',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.grey,
+                            textStyle: TextStyle(color: Colors.white)),
+                        onPressed: () {
+                          outputController.text = '';
+                        },
+                        child: Text('Feld löschen'),
+                      ),
+                    ),
+                    SizedBox(width: 15),
+                    Expanded(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.blue,
+                            textStyle: TextStyle(color: Colors.white)),
+                        onPressed: () async {
+                          final data =
+                              ClipboardData(text: outputController.text);
+                          await Clipboard.setData(data);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: const Text(
+                                  'Daten in die Zwischenablage kopiert'),
+                            ),
+                          );
+                        },
+                        child: Text('in Zwischenablage kopieren'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  String xSalsa20Poly1305IterPbkdf2EncryptToBase64(
+      String password, String iterations, String plaintext) {
+    try {
+      var plaintextUint8 = createUint8ListFromString(plaintext);
+      var passphrase = createUint8ListFromString(password);
+      final PBKDF2_ITERATIONS = int.tryParse(iterations);
+      final salt = PineNaClUtils.randombytes(SecretBox.keyLength);
+      final key = PBKDF2.hmac_sha256(
+          passphrase, salt, PBKDF2_ITERATIONS!, SecretBox.keyLength);
+      final box = SecretBox(key);
+      final ciphertext = box.encrypt(plaintextUint8);
+      final saltBase64 = base64Encoding(salt);
+      //final ivBase64 = base64Encoding(iv); // the iv is integrated in ciphertext
+      final ciphertextBase64 = base64Encoding(ciphertext.asTypedList);
+      return saltBase64 + ':' + ciphertextBase64;
+    } catch (error) {
+      return 'Fehler bei der Verschlüsselung';
+    }
+  }
+
+  Uint8List createUint8ListFromString(String s) {
+    var ret = new Uint8List(s.length);
+    for (var i = 0; i < s.length; i++) {
+      ret[i] = s.codeUnitAt(i);
+    }
+    return ret;
+  }
+
+  String base64Encoding(Uint8List input) {
+    return base64.encode(input);
+  }
+
+  Uint8List base64Decoding(String input) {
+    return base64.decode(input);
+  }
+}
+
+class JsonEncryption {
+  JsonEncryption({
+    required this.algorithm,
+    this.iterations,
+    this.salt,
+    this.iv,
+    required this.ciphertext,
+    this.gcmTag,
+  });
+
+  final String algorithm;
+  final String? iterations;
+  final String? salt;
+  final String? iv;
+  final String ciphertext;
+  final String? gcmTag;
+
+  Map toJson() => {
+        'algorithm': algorithm,
+        'iterations': iterations,
+        'salt': salt,
+        'iv': iv,
+        'ciphertext': ciphertext,
+        'gcmTag': gcmTag,
+      };
+}
